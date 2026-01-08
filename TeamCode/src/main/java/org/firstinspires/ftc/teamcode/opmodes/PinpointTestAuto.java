e1b7dac763b1c19fc3713ed0bed6371c19980ec9
junkjunk123 <havitechrobo@gmail.com>
2026-01-07 22:26:03 -0800

    patches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PinpointTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PinpointTestAuto.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import com.pedropathing.ivy.commands.Infinite;
-import com.pedropathing.ivy.commands.Instant;
-import com.pedropathing.ivy.commands.Wait;
-import com.pedropathing.ivy.commands.WaitUntil;
-import com.pedropathing.ivy.groups.Parallel;
-import com.pedropathing.ivy.groups.Sequential;
+import static com.pedropathing.ivy.commands.Commands.infinite;
+import static com.pedropathing.ivy.commands.Commands.instant;
+import static com.pedropathing.ivy.commands.Commands.waitUntil;
+import static com.pedropathing.ivy.groups.Groups.parallel;
+import static com.pedropathing.ivy.groups.Groups.sequential;
+
+import com.pedropathing.ivy.commands.Commands;
 
 import org.firstinspires.ftc.teamcode.mechanisms.Drivetrain;
 import org.firstinspires.ftc.teamcode.mechanisms.intake.IntakeMotor;
@@ -31,43 +32,42 @@ public class PinpointTestAuto extends OpModeCommand {
         //flywheel = new Flywheel(hardwareMap);
         schedule(
                 //update statements
-                new Infinite(() ->{
+                infinite(() ->{
                     drivetrain.update();
                     telemetry.addData("pose",drivetrain.follower.getPose());
                     telemetry.update();
                 }),
 //                new Infinite(flywheel::update),
 //                new Infinite(intake::update),
-                new WaitUntil(() -> !opModeInInit()),
-                new Sequential(
+                waitUntil(() -> !opModeInInit()),
+                sequential(
                 //following the actual pathing
                 drivetrain.followNext(d -> d.velocityCondition(4)),
-                new Parallel(
-                new Wait(1000),
-                        new Instant(() -> octocanum.engage())
+                parallel(
+                Commands.wait(1000.0),
+                        instant(() -> octocanum.engage())
                         ),
                 drivetrain.followNext(d -> d.velocityCondition(4)),
-                new Parallel(
-                new Wait(1000),
-                        new Instant(() -> octocanum.raise())
+                parallel(
+                Commands.wait(1000.0),
+                        instant(() -> octocanum.raise())
                         ),
                 drivetrain.followNext(d -> d.velocityCondition(4)),
-                        new Wait(1000),
+                        Commands.wait(1000.0),
                 drivetrain.followNext(d -> d.velocityCondition(4)),
-                        new Wait(1000),
+                        Commands.wait(1000.0),
                 drivetrain.followNext(d -> d.velocityCondition(4)),
-                new Wait(1000),
+                Commands.wait(1000.0),
                 drivetrain.followNext(d -> d.velocityCondition(4)),
-                new Wait(1000),
+                Commands.wait(1000.0),
                 drivetrain.followNext(d -> d.velocityCondition(4)),
-                new Wait(1000),
+                Commands.wait(1000.0),
                 drivetrain.followNext(d -> d.velocityCondition(4)),
-                new Wait(1000),
+                Commands.wait(1000.0),
                 drivetrain.followNext(d -> d.velocityCondition(4)),
-                new Wait(1000),
+                Commands.wait(1000.0),
                 drivetrain.followNext(d -> d.velocityCondition(4))
                 )
-
         );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/SortTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/SortTest.java
@@ -1,19 +1,15 @@
 package org.firstinspires.ftc.teamcode.opmodes.test;
 
-import android.graphics.Color;
-
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.mechanisms.intake.ColorManager;
 import org.firstinspires.ftc.teamcode.utils.ArtifactColor;
 import org.firstinspires.ftc.teamcode.utils.RandomizationState;
 import org.firstinspires.ftc.teamcode.mechanisms.intake.Table;
-import org.firstinspires.ftc.teamcode.utils.Types;
 
 
 import java.util.List;
@@ -32,14 +28,13 @@ public class SortTest extends OpMode {
 
     @Override
     public void init() {
-        this.robot = new Robot();
-        robot = new Robot(this);
+        robot = new Robot(hardwareMap);
     }
 
     @Override
     public void start() {
         robot.update();
-        List<Types.TableState> tableStates = List.of(Types.TableState.BALL1, Types.TableState.BALL2, Types.TableState.BALL3);
+        List<Table.RelativeState> tableStates = List.of(Table.RelativeState.BALL0, Table.RelativeState.BALL1, Table.RelativeState.BALL2);
         artifactColors[0] = colorManager.getColorZero();
         artifactColors[2] = colorManager.getColorOne();
         artifactColors[3] = colorManager.getColorTwo();
@@ -56,7 +51,7 @@ public class SortTest extends OpMode {
         index = tableStates.indexOf(robot.table.getState()) - greenIndex + curGreenIndex;
         index = (index + 3) % 3;
 
-        robot.table.getState(tableStates.get(index));
+        robot.table.setPosition(tableStates.get(index).target());
     }
 
 
