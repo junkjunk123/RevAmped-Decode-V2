e1fb3ddde0df6928dc6d130a71e8ca294e823ef5
Error404 <elzethan2018@gmail.com>
2026-02-12 19:45:59 -0800

    resets turntable on teleop start & outtakes when x is pressed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/Tele.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.ivy.commands.Infinite;
 import com.pedropathing.ivy.commands.Instant;
+import com.pedropathing.ivy.commands.Wait;
 import com.pedropathing.ivy.commands.WaitUntil;
 import com.pedropathing.ivy.groups.Parallel;
 import com.pedropathing.ivy.groups.Sequential;
@@ -39,6 +40,7 @@ public class Tele extends OpModeCommand {
     private Robot robot;
     private TeleOpStateHandler tsh;
     private Prompter prompter;
+    private boolean teleReset = false;
 
     @Override
     public void initialize() {
@@ -49,7 +51,7 @@ public class Tele extends OpModeCommand {
         prompter = new Prompter(this, gamepad_1)
                 .prompt("alliance", new StatePrompt<>("Select alliance", AllianceColor.class))
                 .prompt("motif", new StatePrompt<>("Select the motif pattern", RandomizationState.class))
-                .thenDisplay("Good luck! We'r rooting for you. --- Havish & Eric");
+                .thenDisplay("Good luck! We're rooting for you. --- Havish & Eric");
         
         gamepad_1.left_trigger_button(f -> f.greaterThan(0.3f));
         gamepad_1.right_trigger_button(f -> f.greaterThan(0.3f));
@@ -91,6 +93,18 @@ public class Tele extends OpModeCommand {
 
     @Override
     public void execute() {
+        if (!teleReset){
+            schedule(
+            new Sequential(
+                tsh.runTransition(() -> {}, RobotStateHandler.CycleState.SHOOT),
+                tsh.runTransition(
+                    new Sequential(
+                        robot.shootAll(),
+                        robot.resetAfterShooting()
+                    ), RobotStateHandler.CycleState.INTAKE))
+            );
+            teleReset = true;
+        }
         // Update switches
         gamepad_1.update();
         gamepad_2.update();
@@ -150,10 +164,7 @@ public class Tele extends OpModeCommand {
         if (gamepad_2.x.isRisingEdge() && tsh.atState(RobotStateHandler.CycleState.INTAKE) && robot.popper.atState(Popper.PopperState.NEUTRAL)) {
             schedule(tsh.runTransition(
                     new Parallel(
-                            new Instant(() -> {
-                                //robot.tableCompartments.intakeThread.updateColors();
-                                robot.intakeMotor.stop();
-                            }),
+                            new Instant(() -> {robot.intakeMotor.outtake();}),
                             robot.popper.pop(),
                             new Conditional(
                                     robot.flywheel::isStopped,
@@ -215,6 +226,7 @@ public class Tele extends OpModeCommand {
 
         // Telemetry
         telemetry.addData("alliance", Globals.allianceColor);
+        telemetry.addData("tele reset",teleReset);
         telemetry.update();
     }
 
